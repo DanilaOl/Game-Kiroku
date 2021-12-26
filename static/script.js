d69bdb77ca70fb91ca8107f0e1febb725f165ff2
DanilaOl <danilaolyanin@yandex.ru>
2021-12-27 01:46:05 +0300

    second page layout done

--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
-function menuToggler() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function menuToggle() {
+    document.getElementById("menuContent").classList.toggle("show");
+}
+
+function addToToggle() {
+    document.getElementById("addtoContent").classList.toggle("show");
+    document.getElementById("addtoButton").classList.toggle("show");
 }
