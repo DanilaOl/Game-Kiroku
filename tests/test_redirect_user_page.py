b61dd394cce061a4054cd39f0d6cb30e1384f460
DanilaOl <danilaolyanin@yandex.ru>
2022-03-30 16:46:23 +0300

    added search form (not completed)

--- tests/test_redirect_user_page.py
+++ tests/test_redirect_user_page.py
@@ -1,11 +1,16 @@
 import pytest
 
 
-@pytest.mark.parametrize(('name', 'exists'),
-                         [('TestUser', True),
-                          ('TestUserNE', False)])
+# Определение наборов данных
+@pytest.mark.parametrize(('name', 'exists'), [('TestUser', True), ('TestUserNE', False)])
+# Тест функции перехода на страницы пользователя
 def test_redirect_user_page(client, name, exists):
+    # Если предполагается, что пользователь существует
     if exists:
+        # Код состояния ответа сервера должен быть 200
         assert client.get(f'/users/{name}').status_code == 200
+    # Если предполагается, что пользователь не существует
     else:
+        # Код состояния ответа сервера должен быть 404
         assert client.get(f'/users/{name}').status_code == 404
+
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,11 +2,17 @@ import pytest
 from models import get_user
 
 
+# Определение наборов данных
 @pytest.mark.parametrize(('nickname', 'email', 'password', 'password_check'),
                          [('TestUser1', 'test1@test.com', 'qwerty123456', 'qwerty123456'),
                           ('TestUser2', 'test2@test.com', 'loremipsum', 'loremipsum')])
+# Тест функции регистрации
 def test_registration(client, nickname, email, password, password_check):
+    # Посылание POST-запроса с определёнными данными
     client.post('/register', data={'nickname': nickname, 'email': email,
                                    'password': password, 'password_check': password_check})
+    # Запрос зарегистрированного пользователя из БД
     user = get_user(email, password)
+    # Проверка соответствия данных из БД с исходными
     assert user.Nickname == nickname
+
